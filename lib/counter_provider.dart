1ab9e73ed4ee27f4ff556bc247cc73d29e631dbe
MostafaRadian <mostafa.batesta@gmail.com>
2024-10-21 07:54:51 +0300

    Lesson 16

--- lib/counter_provider.dart
+++ lib/counter_provider.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CounterProvider extends ChangeNotifier {
+  int num = 0;
+
   void increment() {
+    num++;
     notifyListeners();
   }
 
   void decrement() {
+    num--;
     notifyListeners();
   }
 }
